da956f5ebcb2d926ba1d0486580e600f35e7b8a9
Nidhi Kumari <Nidhi.Kumari.met14@itbhu.ac.in>
2019-02-24 00:22:13 +0530

    Initial Changes

---
+++ myapp/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+require('./Home.css');
+class Home extends React.Component{
+    render() {
+        return(
+            <div className='c0'>
+                Hey There
+            </div>
+        );
+    }
+}
+export default Home;
